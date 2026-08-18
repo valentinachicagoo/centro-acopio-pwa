7f32416877a998b2a6761557a24fe297c0cda7eb
VCG <vcg@VCGs-iMac.local>
2026-08-18 10:09:41 -0500

    Versionar URLs de assets para romper caché del SW

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'acopio-v4';
-const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.json'];
+const ASSETS = ['/', '/index.html', '/styles.css?v=4', '/app.js?v=4', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
